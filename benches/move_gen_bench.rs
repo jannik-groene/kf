4d44acd6a0fb1273f97a55eb6303ba897e34705d
jgroene <jgroene@student.ethz.ch>
2021-12-21 00:50:23 +0100

    perft test

--- benches/move_gen_bench.rs
+++ benches/move_gen_bench.rs
@@ -1,4 +1,4 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{black_box,criterion_group, criterion_main, Criterion};
 use criterion_cycles_per_byte::CyclesPerByte;
 use kf_internals::chess;
 
@@ -23,7 +23,35 @@ fn do_move_en_passant(c: &mut Criterion<CyclesPerByte>) {
     c.bench_function("do move en passant", |b| b.iter(|| {pos.do_move(m); pos.undo_move();}));
 }
 
-criterion_group!(name = benches;
+fn perft_step(pos: &mut chess::Position, d: u8) -> usize {
+    if d == 0 {
+        1
+    } else if d == 1 {
+        pos.get_moves().len()
+    } else {
+        let mut total = 0;
+        for m in pos.get_moves() {
+            pos.do_move(m);
+            total += perft_step(pos, d-1);
+            pos.undo_move();
+        }
+        total
+    }
+}
+
+pub fn perft(c: &mut Criterion) {
+    let mut pos = chess::Position::new();
+    let d = 4;
+    c.bench_function("perft 4", |b| b.iter( || {
+        perft_step(&mut pos, black_box(d));
+    }));
+}
+
+
+criterion_group!(name = move_gen_benches;
                  config = Criterion::default().with_measurement(CyclesPerByte);
                  targets = move_gen_in_opening,attack_gen_in_opening,do_move_en_passant);
-criterion_main!(benches);
+criterion_group!(name = perft_bench;
+                 config = Criterion::default();
+                 targets = perft);
+criterion_main!(move_gen_benches,perft_bench);
